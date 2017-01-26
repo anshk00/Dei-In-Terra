4fc94ff4d05dd6640eb33feebadaf8cdf79a7687
Anshuman Kumar <anshk00@gmail.com>
2017-01-26 14:27:40 -0600

    Just created shell methods for menu screen.

--- DeiInTerra/DeiInTerra/MenuScreen.cs
+++ DeiInTerra/DeiInTerra/MenuScreen.cs
@@ -11,7 +11,25 @@ namespace DeiInTerra
 {
     class MenuScreen : GameScreen
     {
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
 
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
 
     }
 }
