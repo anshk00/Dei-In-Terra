d012635ba4c881c0aa6cf058a46f3be22f2acfa4
Anshuman Kumar <anshk00@gmail.com>
2017-02-02 14:35:40 -0600

    Implementing button drawings onto menu

--- DeiInTerra/DeiInTerra/Button.cs
+++ DeiInTerra/DeiInTerra/Button.cs
@@ -12,14 +12,28 @@ namespace DeiInTerra
     public class Button
     {
         public Rectangle dimensions {get; private set;}
+        public string buttonLabel { get; private set; }
 
-        public Button (int int1, int int2, int int3, int int4)
+        public Button (int int1, int int2, int int3, int int4, string buttonName)
         {
-            setDimensions(int1, int2, int3, int4);
+            SetDimensions(int1, int2, int3, int4);
+            buttonLabel = buttonName;
         }
-        public void setDimensions(int int1, int int2, int int3, int int4)
+
+        public Button (Vector2 origin, Vector2 size, string buttonName)
+        {
+            SetDimensions(origin, size);
+            buttonLabel = buttonName;
+        }
+
+        public void SetDimensions(int int1, int int2, int int3, int int4)
         {
             dimensions = new Rectangle (int1, int2, int3, int4);
+        }
+
+        public void SetDimensions(Vector2 origin, Vector2 size)
+        {
+            dimensions = new Rectangle(origin.ToPoint(), size.ToPoint())
         }
     }
 }
--- DeiInTerra/DeiInTerra/MenuScreen.cs
+++ DeiInTerra/DeiInTerra/MenuScreen.cs
@@ -7,19 +7,24 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework;
 
+using System.Diagnostics;
+
 namespace DeiInTerra
 {
     class MenuScreen : GameScreen
     {
         private Texture2D menuScreen;
         private Button newGame, loadGame, options, credits, exit;
-        private Vector2 size, origin;
+        private Vector2 size, origin, heightModifier;
+        private SpriteFont menuFont;
+        private List<Button> buttonList = new List<Button>();
+   
         public override void LoadContent()
         {
             base.LoadContent();
             menuScreen = content.Load<Texture2D>("MenuScreen/MenuScreen");
-            size = (259, 40);
-            origin = (283,130);
+            menuFont = content.Load<SpriteFont>("MenuScreen/MenuFont");
+            CreateButtons();
         }
 
         public override void UnloadContent()
@@ -29,13 +34,80 @@ namespace DeiInTerra
 
         public override void Update(GameTime gameTime)
         {
+            CheckMenuClicks();
             base.Update(gameTime);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            base.Draw(spriteBatch);
+            
             spriteBatch.Draw(menuScreen, Vector2.Zero, Color.White);
+            DrawButtons(spriteBatch);
+
+            base.Draw(spriteBatch);
+        }
+
+        public void CreateButtons()
+        {
+            origin = new Vector2(283f, 130f);
+            size = new Vector2(259f, 40f);
+            heightModifier = new Vector2(0f, 80f);
+
+            newGame = new Button(origin, size, "New Game");
+            origin = Vector2.Add(origin, heightModifier);
+            buttonList.Add(newGame);
+
+            loadGame = new Button(origin, size, "Load Game");
+            origin = Vector2.Add(origin, heightModifier);
+            buttonList.Add(loadGame);
+
+            options = new Button(origin, size, "Options");
+            origin = Vector2.Add(origin, heightModifier);
+            buttonList.Add(options);
+
+            credits = new Button(origin, size, "Credits");
+            origin = Vector2.Add(origin, heightModifier);
+            buttonList.Add(credits);
+
+            exit = new Button(origin, size, "Exit");
+            buttonList.Add(exit);
+
+        }
+        public void DrawButtons(SpriteBatch spriteBatch)
+        {
+            foreach(var button in buttonList)
+            {
+                Vector2 stringLocation;
+                spriteBatch.DrawString(menuFont, button.buttonLabel, 
+            }
+        }
+
+        public void CheckMenuClicks()
+        {
+            if (InputManager.Instance.CheckCursorLocation(newGame.dimensions))
+            {
+                Debug.WriteLine("New Game");
+            }
+
+            else if (InputManager.Instance.CheckCursorLocation(loadGame.dimensions))
+            {
+                Debug.WriteLine("Load Game");
+            }
+
+            else if (InputManager.Instance.CheckCursorLocation(options.dimensions))
+            {
+                Debug.WriteLine("Options");
+            }
+
+            else if (InputManager.Instance.CheckCursorLocation(credits.dimensions))
+            {
+                Debug.WriteLine("Credits");
+            }
+
+            else if (InputManager.Instance.CheckCursorLocation(exit.dimensions))
+            {
+                Debug.WriteLine("Exit");
+            }
         }
 
     }
