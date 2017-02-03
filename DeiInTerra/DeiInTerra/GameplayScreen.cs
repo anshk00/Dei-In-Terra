835ab1298add6ebe9fc4fa4a68c98fd54137bb1c
Anshuman Kumar <anshk00@gmail.com>
2017-02-03 14:30:36 -0600

    Started work on player class and gameplay screen.

---
+++ DeiInTerra/DeiInTerra/GameplayScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DeiInTerra
+{
+    class GameplayScreen : GameScreen
+    {
+        Player player;
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            player = new Player(content.Load<Texture2D>("Graphics/Archer class"));
+            player.LoadContent();
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
--- DeiInTerra/DeiInTerra/Player.cs
+++ DeiInTerra/DeiInTerra/Player.cs
@@ -1,19 +1,86 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 
 namespace DeiInTerra
 {
     internal class Player : Character
     {
-        public Player(Texture2D a, float ScreenWidth, float ScreenHeight)
+        public Vector2 Velocity;
+        public float MoveSpeed;
+
+
+        public Player(Texture2D playerTexture)
         {
+            Velocity = Vector2.Zero;
             health = 100;
             totalHealth = 100;
             mana = 100;
             totalMana = 100;
-            model = a;
+            model = playerTexture;
         }
 
-        public int skillPoints
+        public void LoadContent()
+        {
+
+        }
+
+        public void UnloadContent()
+        {
+
+        }
+        
+        public void Update(GameTime gameTime)
+        {
+            
+                
+
+
+        }
+
+        public void UpdateVelocity(GameTime gameTime)
+        {
+            if(Velocity.X ==0)
+            {
+                if (InputManager.Instance.KeyDown(Keys.S, Keys.Down))
+                    Velocity.Y = MoveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                else if (InputManager.Instance.KeyDown(Keys.Up, Keys.W))
+                {
+                    Velocity.Y = -MoveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+                else
+                {
+                    Velocity.Y = 0;
+                }
+            }
+
+            if(Velocity.Y ==0)
+            {
+                if (InputManager.Instance.KeyDown(Keys.Right, Keys.D))
+                {
+                    Velocity.X = MoveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+                else if (InputManager.Instance.KeyDown(Keys.Left, Keys.A))
+                {
+                    Velocity.X = -MoveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+                else
+                {
+                    Velocity.X = 0;
+                }
+            }
+
+
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public int SkillPoints
         {
             get
             {
@@ -23,26 +90,6 @@ namespace DeiInTerra
             set
             {
             }
-        }
-
-        public void setDirection()
-        {
-        }
-
-        public void moveLeft()
-        {
-        }
-
-        public void moveRight()
-        {
-        }
-
-        public void moveJump()
-        {
-        }
-
-        public void adjustStats()
-        {
         }
     }
 }
